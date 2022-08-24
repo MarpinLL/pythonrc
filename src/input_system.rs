d2ab67649c8104dbd8d3b7e9652baaaa953bfd48
Marcos López Lamas <mll2725@gmail.com>
2022-08-24 23:43:15 +0200

    Added funtion to get lexeme from de doble buffer. Added funtions for moving the forward index forward or backwards

--- src/input_system.rs
+++ src/input_system.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::Read;
 use crate::Config;
 
-const BUFFER_SIZE: usize = 10;
+const BUFFER_SIZE: usize = 50;
 const BUFFER_A_EOF: usize = BUFFER_SIZE - 1;
 const BUFFER_B_EOF: usize = (2 * BUFFER_SIZE) - 1;
 
@@ -10,13 +10,13 @@ pub struct DoubleBuffer {
     file: File,
     buffer: [u8; 2 * BUFFER_SIZE],
     begin: usize,
-    forward: usize,
+    forward: isize,
 }
 
 impl DoubleBuffer {
     pub fn new(config: Config) -> Result<DoubleBuffer, &'static str> {
         let begin = 0;
-        let forward = 0;
+        let forward = -1;
 
         let mut file = match File::open(config.filename) {
             Ok(file) => file,
@@ -32,12 +32,50 @@ impl DoubleBuffer {
 
         Ok(DoubleBuffer { file, buffer, begin, forward })
     }
+
+    pub fn get_lexeme(&mut self) -> String {
+        let mut lexeme = String::new();
+        let mut i = self.begin as isize;
+
+        if i == self.forward{
+            let c = self.buffer[i] as char;
+            lexeme.push(c);
+        }
+
+        while i != self.forward {
+            if self.buffer[i] != 0u8 {
+                let c = self.buffer[i] as char;
+                lexeme.push(c);
+            }
+
+            i = (i + 1) % BUFFER_SIZE;
+        }
+
+        self.next();
+        self.begin = self.forward as usize;
+        self.back();
+
+        lexeme
+    }
+
+    pub fn back(&mut self) {
+        let index = if self.forward == 0 {
+            BUFFER_B_EOF - 1
+        } else if self.forward == (BUFFER_A_EOF + 1) as isize {
+            BUFFER_A_EOF - 1
+        } else {
+            self.forward - 1
+        };
+
+        self.forward = index as isize;
+    }
 }
 
 impl Iterator for DoubleBuffer {
     type Item = Result<char, &'static str>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.forward = self.forward + 1;
         let c = match self.buffer[self.forward] {
             /*
              * If EOF (represented by the byte 0) is found, we need to find out if it's
@@ -46,7 +84,7 @@ impl Iterator for DoubleBuffer {
              * End of buffer -> Load the other buffer and move the forward index.
              */
             0u8 => {
-                if self.forward == BUFFER_A_EOF {
+                if self.forward as usize == BUFFER_A_EOF {
                     // Load second buffer reading the next characters
                     let n = match self.file.read(&mut self.buffer[BUFFER_SIZE..BUFFER_B_EOF]) {
                         Ok(n) => (n),
@@ -62,7 +100,7 @@ impl Iterator for DoubleBuffer {
 
                     // Move the index to the start of the second buffer
                     self.forward = self.forward + 1;
-                } else if self.forward == BUFFER_B_EOF {
+                } else if self.forward as usize == BUFFER_B_EOF {
                     // Load first buffer reading the next characters
                     let n = match self.file.read(&mut self.buffer[0..BUFFER_A_EOF]) {
                         Ok(n) => (n),
@@ -87,7 +125,6 @@ impl Iterator for DoubleBuffer {
             _ => self.buffer[self.forward] as char
         };
 
-        self.forward = self.forward + 1;
         Some(Ok(c))
     }
 }
